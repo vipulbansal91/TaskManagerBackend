12c6604b09fa5a014b4522cac6d69e27b71ace6d
vipulbansal91 <vipulbansal91@gmail.com>
2017-01-04 07:22:43 +0530

    Adding maven + lombok

--- src/main/java/taskManager/activity/GetTasks.java
+++ src/main/java/taskManager/activity/GetTasks.java
@@ -1,9 +1,14 @@
 package taskManager.activity;
 
+import taskManager.model.Task;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vbbansal on 1/1/17.
@@ -12,7 +17,7 @@ import javax.ws.rs.QueryParam;
 public class GetTasks
 {
     @GET
-    @Produces("text/plain")
+    @Produces(MediaType.APPLICATION_JSON)
     public String getTasks(@QueryParam("date") String date)
     {
         if (date == null)
